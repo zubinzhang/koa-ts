717b0f46d6ce29bb5625990064cf2003b70f8b71
zubin <zubincheung@foxmail.com>
2017-11-15 09:52:38 +0800

    去除try catch

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -20,42 +20,43 @@ export function handleError(): Middleware {
     appLog.info(`当前headers:${JSON.stringify(ctx.headers)}`);
     appLog.info(`当前query:${JSON.stringify(ctx.request.query)}`);
     appLog.info(`当前body:${JSON.stringify(ctx.request.body)}`);
-    try {
-      await next();
-    } catch (err) {
+    // try {
+    await next();
+    // } catch (err) {
 
-      // 参数校验错误
-      if (err instanceof ValidationError) {
-        err.retCode = retCodeEnum.serverError;
-        err.errCode = errCodeEnum.paramTypeError;
-      }
+    //   // 参数校验错误
+    //   if (err instanceof ValidationError) {
+    //     err.retCode = retCodeEnum.serverError;
+    //     err.errCode = errCodeEnum.paramTypeError;
+    //   }
 
-      ctx.body = formatResData(
-        null,
-        err.retCode || retCodeEnum.serverError,
-        err.errCode || errCodeEnum.autoSnapError,
-        err.message || err.toString(),
-      );
+    //   ctx.body = formatResData(
+    //     null,
+    //     err.retCode || retCodeEnum.serverError,
+    //     err.errCode || errCodeEnum.autoSnapError,
+    //     err.message || err.toString(),
+    //   );
 
 
-      appLog.error(`出现异常错误:${err.toString()}`);
-      appLog.error(`错误堆栈：${err.stack}`);
-      appLog.error(`响应数据:${JSON.stringify(ctx.body)}`);
-    } finally {
-      // 保存post请求的日志
-      if (ctx.req.method.toUpperCase() === 'POST') {
-        const urls = ctx.url.split('/');
+    //   appLog.error(`出现异常错误:${err.toString()}`);
+    //   appLog.error(`错误堆栈：${err.stack}`);
+    //   appLog.error(`响应数据:${JSON.stringify(ctx.body)}`);
+    // } finally {
+    //   // 保存post请求的日志
+    //   if (ctx.req.method.toUpperCase() === 'POST') {
+    //     const urls = ctx.url.split('/');
 
-        // mongoModel.logger.create({
-        //   url: ctx.url,
-        //   createAt: moment().format('YYYY-MM-DD HH:mm:ss'),
-        //   header: ctx.headers,
-        //   reqData: ctx.request.body,
-        //   resData: ctx.body,
-        //   name: urls[urls.length - 1],
-        // }).catch(() => true);
-      }
-      appLog.info('=======end:结束本次请求跟踪=======\n');
-    }
+    //     // mongoModel.logger.create({
+    //     //   url: ctx.url,
+    //     //   createAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+    //     //   header: ctx.headers,
+    //     //   reqData: ctx.request.body,
+    //     //   resData: ctx.body,
+    //     //   name: urls[urls.length - 1],
+    //     // }).catch(() => true);
+    //   }
+    //   appLog.info('=======end:结束本次请求跟踪=======\n');
+    // }
+    appLog.info('=======end:结束本次请求跟踪=======\n');
   };
 }
