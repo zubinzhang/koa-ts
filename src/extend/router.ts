43a46157127271417c5f35d86b0acba45f7d4076
Zubin Cheung <zubin@zubin-pc.local>
2018-07-05 14:32:35 +0800

    路由

--- src/extend/router.ts
+++ src/extend/router.ts
@@ -10,21 +10,37 @@ import { join } from 'path';
 
 const router = new KoaRouter();
 
+/**
+ * 路由
+ *
+ * @export
+ * @class Router
+ */
 export default class Router {
+  // 用于存储路由信息
   static routerSet: Set<{
     method: string;
     path: string;
     middlewares: Koa.Middleware[];
   }> = new Set();
 
+  /**
+   * 初始化路由
+   *
+   * @static
+   * @returns
+   * @memberof Router
+   */
   static init() {
     // 加载所有控制器
     glob.sync(join(__dirname, '../controller/**/*.js')).forEach(require);
 
+    // 挂载路由
     for (const { method, path, middlewares } of this.routerSet) {
       router[method](path, ...middlewares);
     }
 
+    // 404
     router.all('*', (ctx: Koa.Context) => {
       ctx.status = 404;
       ctx.error('Router Not Found');
